f631eb0a934b78c4867a74a65dc914de68d19d72
yRadioactive <niolt2903@gmail.com>
2025-10-07 19:20:30 +0200

    a

--- Haskell/tupel/tupel.hs
+++ Haskell/tupel/tupel.hs
@@ -1,2 +1,10 @@
 swap :: (Int, String) -> (String,Int)
 swap (x,y) = (y,x)
+
+add_frac :: ((Int,Int),(Int,Int)) -> (Int,Int)
+add_frac ((x,y),(v,w)) = ((x*w)+(y*v),(y*w))
+
+add_frac_cancel :: ((Int,Int),(Int,Int)) -> (Int,Int) 
+add_frac_cancel ((x,y),(v,w)) = ((s `div` (gcd s t)),(t `div` (gcd s t))) 
+    where   s =(x*w)+(y*v)
+            t= (y*w)

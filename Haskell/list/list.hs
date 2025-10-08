f49de5a2f1ca6c7a2172bc22e05f430b9687a961
yRadioactive <niolt2903@gmail.com>
2025-10-08 19:20:17 +0200

    aa

--- Haskell/list/list.hs
+++ Haskell/list/list.hs
@@ -16,3 +16,8 @@ every_other x = if null x then []
                 else if (null (tail x)) then [head x] 
                 else [(head x)]++(every_other (tail(tail x))) 
 
+sorted :: [Int] -> Bool
+sorted x =  if  null x then True
+            else if null (tail x) then True
+            else if (head x) <= head (tail x) then sorted (tail x)
+            else False

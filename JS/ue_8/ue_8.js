258e80540ef091744b110ac925016c5006f6dab4
yRadioactive <niolt2903@gmail.com>
2025-11-26 21:20:33 +0100

    asd

--- JS/ue_8/ue_8.js
+++ JS/ue_8/ue_8.js
@@ -1,9 +1,22 @@
 function testOverflow() {
     var x = Number.MIN_SAFE_INTEGER;
     var y = Number.MAX_SAFE_INTEGER;
-    x--;
-    y++;
-    print(x);
-    print(y);
+    console.log(x);
+    console.log(y);
+    console.log("________________________");
+    
+    x=x**x;
+    y=y**y;
+    console.log(x);
+    console.log(y);
 }
-testOverflow();
+
+function testExpo() {
+    for (let i = 1; i > 0; i=i-0.1) {
+        
+        console.log(i**i);
+    }
+}
+
+//testOverflow();
+testExpo();

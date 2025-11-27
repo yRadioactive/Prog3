b1b48400372aecc2847290e801d0c12ffcd69201
yRadioactive <niolt2903@gmail.com>
2025-11-27 21:50:38 +0100

    ich kann nicht mehr

--- JS/ue_8/ue_8.js
+++ JS/ue_8/ue_8.js
@@ -4,19 +4,44 @@ function testOverflow() {
     console.log(x);
     console.log(y);
     console.log("________________________");
-    
-    x=x**x;
-    y=y**y;
+
+    x = x ** x;
+    y = y ** y;
     console.log(x);
     console.log(y);
 }
 
 function testExpo() {
-    for (let i = 1; i > 0; i=i-0.1) {
-        
-        console.log(i**i);
+    for (let i = 1; i > 0; i = i - 0.1) {
+
+        console.log(i ** i);
     }
 }
 
+function combineArrays() {
+    var output = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {
+        output = output.concat(arguments[i]);
+    }
+    output = output.filter(v => ["number","boolean","string"].includes(typeof v));
+
+    output.sort();
+
+    output = [...new Set(output)]
+
+    return output;
+}
+
+const arr1 = [1, 2, 3, 1, 4];
+const arr2 = [5, 6, 7, 8, null];
+const arr3 = ["was","soll","das"];
+const arr4 = ["was","spl","Das","WAS","SPL"];
+
+
+//function parse(filename,filesize,type){
+//}
+
 //testOverflow();
-testExpo();
+//testExpo();
+console.log(combineArrays(arr1, arr2,arr3,arr4));
